0c3962b7c7e452e7cbb9fae79f2fddb8147b2fb3
francescomascellino <franesco.mascellino@gmail.com>
2023-09-01 15:02:07 +0200

    add class list check on btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,44 @@
 const generateBtn = document.getElementById("generateBtn");
+const fieldElement = document.querySelector(".field");
+const limit = 100;
 
 console.log(generateBtn);
 
 generateBtn.innerHTML = "Casa"
+
+function generateFieldExercise(domElement, limit) {
+
+    for (let i = 0; i < limit; i++) {
+
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('cell');
+        domElement.append(cellElement);
+        const cellValue = i + 1;
+
+        //l'addEventListener viene associato a ogni cella ogni volta che viene creata durante il ciclo e rimane in attesa.
+        cellElement.addEventListener("click", function () {
+
+            //this fa rifermento al "soggetto" dell'eventListener
+            this.classList.toggle("bgGreen");
+            console.log(cellValue);
+            cellElement.innerHTML = "";
+
+        })
+
+    }
+};
+
+generateFieldExercise(fieldElement, limit);
+
+generateBtn.addEventListener("click", function () {
+
+    generateBtn.classList.toggle("active");
+    console.log(generateBtn);
+
+    if (generateBtn.classList.contains("active")) {
+        console.log("active");
+    } else {
+        console.log("absent");
+    }
+
+});
